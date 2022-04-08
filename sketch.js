bef67d9b4fbce05ed334b60afcd95bfb94cb20c4
Erol Bickici <erol@ErolsMiiMacbook.fios-router.home>
2022-04-08 14:09:26 -0400

    dynamic canvas set on the eq sky sphere w/ updated photo

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
 // Main
 
 // Main Inits
+
+// options for scaling
+// 1. photoshop individual objects
+// 2. recolor sky spehre specific objects and just switch out
+// 3. use ray tracing (interactiove dynamic texture) and draw on skysphere which is a canvas
+//          draw img onto canvas and just draw
 let world
+let myImage = null
 
 const roomPoints = [
     { name: "point1", asset: "church" },
@@ -10,30 +17,86 @@ const roomPoints = [
 
 const markers = []
 
+let buffer1
+let texture1
+
+let imgWidth = 6080
+let imgHeight = 3040
+
+let myFactor = 2
+
 // Setup
 function setup() {
-    noCanvas()
+    // no main canvas - we will just use our off screen graphics buffers to hold our dynamic textures
+    noCanvas();
 
-    // Init World
-    world = new World('VRScene')
-    world.setBackground(0, 0, 0)
-    world.add(new Sky({ asset: 'church' }))
+    // construct the A-Frame world
+    // this function requires a reference to the ID of the 'a-scene' tag in our HTML document
+    world = new World('VRScene');
 
-    world.setFlying(true)
+    // set the background color of the world
+    world.setBackground(0, 0, 0);
 
-    markers.push(new Marker({ x: 0, y: 3, z: 0, name: "Something", description: "Idk" }))
+    myImage = loadImage('./assets/roomtest.JPG')
+
+    // create our off screen graphics buffer & texture
+    buffer1 = createGraphics(imgWidth / myFactor, imgHeight / myFactor);
+    texture1 = world.createDynamicTextureFromCreateGraphics(buffer1);
+    buffer1.ellipse(0, 0, 50, 50)
+    // buffer1.background(0)
+    buffer1.clear()
+    let skySphere = new Sphere({
+        x: 0, y: 0, z: 0,
+        radius: 100,
+        asset: texture1,
+        dynamicTexture: true,
+        transparent: true,
+        dynamicTextureWidth: imgWidth / myFactor,
+        dynamicTextureHeight: imgHeight / myFactor,
+        overFunction: function (entity, intersectionInfo) {
+            // intersectionInfo is an object that contains info about how the user is
+            // interacting with this entity.  it contains the following info:
+            // .distance : a float describing how far away the user is
+            // .point3d : an object with three properties (x, y & z) describing where the user is touching the entity
+            // .point2d : an object with two properites (x & y) describing where the user is touching the entity in 2D space (essentially where on the dynamic canvas the user is touching)
+            // .uv : an object with two properies (x & y) describing the raw textural offset (used to compute point2d)
+
+            // draw an ellipse at the 2D intersection point on the dynamic texture
+            // buffer1.fill(random(255), random(255), random(255));
+            // buffer1.ellipse(intersectionInfo.point2d.x, intersectionInfo.point2d.y, 20, 20);
+            // console.log("here");
+            // console.log(intersectionInfo.point2d.x, intersectionInfo.point2d.y)
+        }
+    });
+
+    world.add(new Sky({
+        asset: "roomtest",
+    }))
+    world.add(skySphere);
+
 }
 
-// Main Draw
 function draw() {
-    for (let i = 0; i < markers.length; i++) {
-
-    }
+    // buffer1.image(myImage, 0, 0)
+    // buffer1.fill('red')
+    // buffer1.ellipse(random(0, imgWidth / 2), random(0, imgHeight / 2), 50, 50)
 }
+
+
+// var startColor = color(
+//     floor(random(255)),
+//     floor(random(255)),
+//     floor(random(255))
+// );
+// var endColor = color(
+//     floor(random(255)),
+//     floor(random(255)),
+//     floor(random(255))
+// );
 
 class Marker {
     constructor({ x, y, z, name, description }) {
-        this.obj = new Box({
+        this.obj = new Sphere({
             x, y, z,
             width: 1,
             depth: 1.5,
@@ -53,5 +116,12 @@ class Marker {
             }
         })
         world.add(this.obj)
+    }
+    display() {
+        this.obj.scale()
+        if (frameCount % 240 === 0) {
+            console.log("YUP")
+            // this.obj.color(lerpColor(startColor, endColor, 0))
+        }
     }
 }
